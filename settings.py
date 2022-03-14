891989e899100f5c2257f304b44bbfcfc2fbf1d3
RealMuffinTime <realmuffintime@gmail.com>
2022-03-14 12:58:07 +0100

    init settings.py

---
+++ settings.py
@@ -0,0 +1,20 @@
+# Here you can specify master bot id/token
+master_id = ""
+master_token = ""
+
+# Here you can specify dev bot id/token
+dev_id = ""
+dev_token = ""
+
+# Here you can set a id for a guild on which you test the bot, you can upload the commands to this specific guild
+guild_id = ""
+
+# Here in this list you can define the commands you want to upload/update
+command_list = [{"name": "", "description": ""},
+                {"name": "", "description": ""},
+                {"name": "", "description": ""},
+                {"name": "", "description": ""},
+                {"name": "", "description": ""}]
+
+# Here you can specify id you got from get requests to delete commands
+delete_list = ["", "", "", "", ""]
